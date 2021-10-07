7ee15116534c0bbd3b69d13ecebfcc05812a5bd9
ensar <ensar@DESKTOP-PJ52DT8>
2021-10-07 23:14:36 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string MaintenanceTime = "Sistem bakımda.";
+        public static string ProductsListed ="Ürünler listelendi.";
+        public static string ProductCountOfCategoryError="Bir kategoride en fazla 20 ürün olabilir.";
+        public static string ProductUpdated="Ürün güncellendi.";
+        public static string ProductNameAlreadyExists="Bu isimde bir ürün vardır.";
+        public static string CategoryLimitExceded="Kategori limiti 15 olduğu için ürün ekleyemezsiniz.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kullanıcı kayıtlı";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string SuccessfulLogin = "Başarılı giriş";
+        public static string PasswordError = "Şifre hatalı";
+        public static string UserAlreadyExists = "Kullanıcı zaten var";
+        public static string AccessTokenCreated = "Access token oluşturuldu";
+    }
+}
